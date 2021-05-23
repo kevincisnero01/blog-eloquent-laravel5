b41c9b7e4786351b52a3771034fb101be2915f3e
kevincisnero01 <kevingabriel18@gmail.com>
2021-05-23 16:21:22 -0430

    Config view welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,15 +79,13 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Eloquent Avanzado
                 </div>
-
+            
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    @foreach($users as $user)
+                        <a href="#">{{$user->name}}</a>
+                    @endforeach
                 </div>
             </div>
         </div>
