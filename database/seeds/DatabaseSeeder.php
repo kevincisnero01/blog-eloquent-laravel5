f3fce589dab1a04df891c52c6356133e1c2e4cc7
kevincisnero01 <kevingabriel18@gmail.com>
2021-05-28 22:56:56 -0430

    Seeder de Usuarios (Profile, Location, Image & Groups)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,5 +17,27 @@ class DatabaseSeeder extends Seeder
         factory(App\Level::class)->create(['name' => 'Oro']);
         factory(App\Level::class)->create(['name' => 'Plata']);
         factory(App\Level::class)->create(['name' => 'Bronce']);
+
+        factory(App\User::class,5)->create()each(function ($user){
+
+            $perfil = $user->profile->save(factory(App\Profile::class)->make());
+
+            $profile->location()->save(factory(App\Location::class)->make());
+            $user->groups()->attach($this->array(rand(1,3)));
+
+            $user->image()->save(factory(App\Image::classs)->make([ 'url' => 'http://lorempixel.com/90/90']));
+        });
+
+    }
+
+    public function array($max)
+    {
+        $values = [];
+
+        for ($i=1; $i < $max; $i++) { 
+            $values[] = $i;
+        }
+
+        return $values;
     }
 }
