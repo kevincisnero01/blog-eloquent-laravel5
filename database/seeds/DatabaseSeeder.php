5efd3e604deed9c5370119c76108cf8f994f3d73
kevincisnero01 <kevingabriel18@gmail.com>
2021-05-30 14:58:22 -0430

    Seeder de Posts y Videos (Category, Tags & Comments)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -28,6 +28,36 @@ class DatabaseSeeder extends Seeder
             $user->image()->save(factory(App\Image::classs)->make([ 'url' => 'http://lorempixel.com/90/90']));
         });
 
+        factory(App\Category::class,4)->create();
+
+        factory(App\Tag::class,12)->create();
+
+        factory(App\Post::class,40)->create()->each(function ($post){
+
+            $post->image()->save(factory(App\Image::class)->make());
+
+            $post->tags()->attach($this->array(rand(1,12)));
+
+            $number_comments = rand(1,6);
+
+            for ($i=0; $i < $number_comments; $i++) { 
+                $post->comments()->save(factory(App\Comment::class)->make());
+            }
+        });
+
+        factory(App\Video::class,40)->create()->each(function ($video){
+
+            $video->image()->save(factory(App\Image::class)->make());
+
+            $video->tags()->attach($this->array(rand(1,12)));
+
+            $number_comments = rand(1,6);
+
+            for ($i=0; $i < $number_comments; $i++) { 
+                $video->comments()->save(factory(App\Comment::class)->make());
+            }
+        });
+
     }
 
     public function array($max)
